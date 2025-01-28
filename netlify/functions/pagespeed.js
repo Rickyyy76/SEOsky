b9f607e52c945bdd5be7bff8f0396504799fb52b
Rickyyy76 <Rick-Schmiedt@web.de>
2025-01-28 23:17:30 +0100

    Update pagespeed.js

--- netlify/functions/pagespeed.js
+++ netlify/functions/pagespeed.js
@@ -1,30 +1,28 @@
-// netlify/functions/pagespeed.js
-
 const axios = require('axios');
 
 exports.handler = async function(event, context) {
-  const { API_KEYS } = process.env;  // API-Schlüssel aus den Umgebungsvariablen
+    const apiKey = process.env.PAGESPEED_API_KEY;  // Dein API-Schlüssel aus den Umgebungsvariablen
+    const url = event.queryStringParameters.url;
 
-  const url = event.queryStringParameters.url;
-  if (!url) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ message: "URL parameter is required" }),
-    };
-  }
+    if (!url) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: "URL-Parameter wird benötigt" }),
+        };
+    }
 
-  const apiUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}&key=${API_KEYS}`;
+    const apiUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}&key=${apiKey}`;
 
-  try {
-    const response = await axios.get(apiUrl);
-    return {
-      statusCode: 200,
-      body: JSON.stringify(response.data),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: "Error fetching PageSpeed data", error: error.message }),
-    };
-  }
+    try {
+        const response = await axios.get(apiUrl);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response.data),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Fehler beim Abrufen der PageSpeed-Daten", error: error.message }),
+        };
+    }
 };
