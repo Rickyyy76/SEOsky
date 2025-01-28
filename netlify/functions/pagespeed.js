c03a16980cfd5ab0cda0622063285eb2bbb4c9a0
Rickyyy76 <Rick-Schmiedt@web.de>
2025-01-28 22:13:30 +0100

    Add files via upload

---
+++ netlify/functions/pagespeed.js
@@ -0,0 +1,30 @@
+// netlify/functions/pagespeed.js
+
+const axios = require('axios');
+
+exports.handler = async function(event, context) {
+  const { API_KEY } = process.env;  // API-Schlüssel aus den Umgebungsvariablen
+
+  const url = event.queryStringParameters.url;
+  if (!url) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: "URL parameter is required" }),
+    };
+  }
+
+  const apiUrl = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${url}&key=${API_KEY}`;
+
+  try {
+    const response = await axios.get(apiUrl);
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Error fetching PageSpeed data", error: error.message }),
+    };
+  }
+};
